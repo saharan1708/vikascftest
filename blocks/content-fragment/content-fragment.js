a4083b0c6e64a2194e0adcdd82633565af9140ba
saharan1708 <164372032+saharan1708@users.noreply.github.com>
2024-07-23 10:27:42 +0530

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,5 +1,5 @@
 // put your AEM publish address here
-const AEM_HOST = "https://publish-p131639-e1282833.adobeaemcloud.com";
+const AEM_HOST = "https://publish-p121371-e1189853.adobeaemcloud.com";
 
 export default function decorate(block) {
 
